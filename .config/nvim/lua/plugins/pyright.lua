a4e32d1154161e36f7d8ca15c4f170ceff0c6997
Juan Manuel Schillaci <jmschillaci@gmail.com>
2025-07-17 19:13:35 -0300

    Updated lazyvim

---
+++ .config/nvim/lua/plugins/pyright.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {
+          settings = {
+            pyright = {
+              disableOrganizeImports = true, -- Using Ruff
+            },
+            python = {
+              analysis = {
+                ignore = { "*" }, -- Using Ruff
+                typeCheckingMode = "off", -- Using mypy
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+}
